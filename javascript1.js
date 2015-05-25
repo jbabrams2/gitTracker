b6eeba048674d86b786747ec716e42b8d988507d
jbabrams2 <jbabrams2@gmail.com>
2015-05-25 14:29:47 -0500

    updated UI, added new users to track (4 total), deleted conflicting files (like old CSS and Index files)

--- javascript1.js
+++ javascript1.js
@@ -1,4 +1,4 @@
-$("#btn_get_repos").click(function() {
+function countPushes() {
             var arr1 = [];                                                //array that contains the dates
             var match = [];                                               //array that eliminates duplicate dates
             var arr2 = [];                                                //array that contains the count of pushes for each date (minus duplicates)
@@ -30,23 +30,19 @@ $("#btn_get_repos").click(function() {
                                                                           //All dates must be stored in this array so that the above IF statement can 
                                                                           //cross reference datePushed with previous dates to determine whether it's a duplicate.
             }
-                console.log(arr1);                                        //print result in log
-                console.log(match);                                       //print result in log
-                console.log(arr2);                                        //print result in log
+                // console.log(arr1);                                        //print result in log
+                // console.log(match);                                       //print result in log
+                // console.log(arr2);                                        //print result in log
             for (var z = 0; z < match.length; z++) {                      // loop through the Match array
                 if(arr2[z] == "1"){
-                    $("#repo_name").append(                               //if arr2[z] is equal to 1,
-                     "<div>" + arr2[z] + " push on</div>"                 //write "push" instead of "pushes" (just for proper english)
+                    $("#pushList1").append(                               //if arr2[z] is equal to 1,
+                     "<li>" + arr2[z] + " push on " + match[z] + "</li>"                 //write "push" instead of "pushes" (just for proper english)
                  );                                                       //also here, the main purpose is to print the number stored in arr2[z] in the view
                 }else{
-                    $("#repo_name").append(
-                     "<div>" + arr2[z] + " pushes on</div>"
+                    $("#pushList1").append(
+                     "<li>" + arr2[z] + " pushes on " + match[z] + "</li>"
                  );
                 }
-                 $("#repo_date").append(                                  //print the date that arr2[z] corresponds to.
-                    "<div> " + match[z] + "</div>"                        //end result should be: "x pushes on [date]"
-                                                                          //example: 3 pushes on 2015-03-13
-                    );
             }
         }
     });
@@ -81,24 +77,110 @@ $("#btn_get_repos").click(function() {
                                                                           //All dates must be stored in this array so that the above IF statement can 
                                                                           //cross reference datePushed with previous dates to determine whether it's a duplicate.
             }
-                console.log(arr1v2);                                      //print result in log
-                console.log(matchv2);                                     //print result in log
-                console.log(arr2v2);                                      //print result in log
+
             for (var z = 0; z < matchv2.length; z++) {                    // loop through the Match array
                 if(arr2v2[z] == "1"){
-                    $("#repo_name4").append(                              //if arr2[z] is equal to 1,
-                     "<div>" + arr2v2[z] + " push on</div>"               //write "push" instead of "pushes" (just for proper english)
+                    $("#pushList2").append(                               //if arr2[z] is equal to 1,
+                     "<li>" + arr2v2[z] + " push on " + matchv2[z] + "</li>"                 //write "push" instead of "pushes" (just for proper english)
                  );                                                       //also here, the main purpose is to print the number stored in arr2[z] in the view
                 }else{
-                    $("#repo_name4").append(
-                     "<div>" + arr2v2[z] + " pushes on</div>"
+                    $("#pushList2").append(
+                     "<li>" + arr2v2[z] + " pushes on " + matchv2[z] + "</li>"
                  );
+              }
+          }
+        }
+    });
+
+//-------------------------------- call to third github user -------------------------- \\
+
+            var arr1v3 = [];                                              //array that contains the dates
+            var matchv3 = [];                                             //array that eliminates duplicate dates
+            var arr2v3 = [];   
+    $.ajax({                                             
+        type: "GET",
+        url: "https://api.github.com/users/kevinsawicki/repos",               //connect to Github API
+        dataType: "json",
+        success: function(result) {
+            for (var e = 0; e < result.length; e++)  {                    //for each result from api call:
+                var pushed_at = result[e].pushed_at;                      //assign the API value: "pushed_at" to "pushed_at" variable
+                var datePushed = pushed_at.substr(0, 10);                 //eleminate any text in the string that comes after the date and assign it to variable "datePushed"
+                var count = 0;                                            //stores the number of times a user pushed code on each date
+                 for (var x = 0; x < result.length; x++) {                //start another loop that runs through the length of the "result" object.
+                        var Match_pushed_at = result[x].pushed_at;        //assign the API value: "pushed_at" to "pushed_at" variable
+                        var dateMatched = Match_pushed_at.substr(0, 10);  //eleminate any text in the string that comes after the date and assign it to variable "datePushed"
+                        if(datePushed == dateMatched){                    //if the current value of loop 1 is equal to the value of loop 2,
+                                                                          // it means that the user again pushed code on this date. 
+                        count++;                                          //Add 1 to the "count" variable to document this.
+                        }        
+                    }
+                if(arr1v3.indexOf(datePushed) < 1){                       //if the date doesn't already exist in "arr1" array, it isn't a duplicate.
+                    matchv3.push(datePushed);                             //since it isn't a duplicate, push it to "Match" array, and
+                    arr2v3.push(count.toString())                         //push the number stored in the "count" variable to the "arr2" array
                 }
-                 $("#repo_date5").append(                                 //print the date that arr2[z] corresponds to.
-                    "<div> " + matchv2[z] + "</div>"                      //end result should be: "x pushes on [date]"
-                                                                          //example: 3 pushes on 2015-03-13
-                    );
+                arr1v3.push(datePushed);                                  //no matter what, add datePushed to array 1. 
+                                                                          //All dates must be stored in this array so that the above IF statement can 
+                                                                          //cross reference datePushed with previous dates to determine whether it's a duplicate.
             }
+
+            for (var z = 0; z < matchv3.length; z++) {                    // loop through the Match array
+                if(arr2v3[z] == "1"){
+                    $("#pushList3").append(                               //if arr2[z] is equal to 1,
+                     "<li>" + arr2v3[z] + " push on " + matchv3[z] + "</li>"                 //write "push" instead of "pushes" (just for proper english)
+                 );                                                       //also here, the main purpose is to print the number stored in arr2[z] in the view
+                }else{
+                    $("#pushList3").append(
+                     "<li>" + arr2v3[z] + " pushes on " + matchv3[z] + "</li>"
+                 );
+              }
+          }
+        }
+    });
+
+//-------------------------------- call to fourth github user -------------------------- \\
+
+            var arr1v4 = [];                                              //array that contains the dates
+            var matchv4 = [];                                             //array that eliminates duplicate dates
+            var arr2v4 = [];   
+    $.ajax({                                             
+        type: "GET",
+        url: "https://api.github.com/users/brianchandotcom/repos",               //connect to Github API
+        dataType: "json",
+        success: function(result) {
+            for (var e = 0; e < result.length; e++)  {                    //for each result from api call:
+                var pushed_at = result[e].pushed_at;                      //assign the API value: "pushed_at" to "pushed_at" variable
+                var datePushed = pushed_at.substr(0, 10);                 //eleminate any text in the string that comes after the date and assign it to variable "datePushed"
+                var count = 0;                                            //stores the number of times a user pushed code on each date
+                 for (var x = 0; x < result.length; x++) {                //start another loop that runs through the length of the "result" object.
+                        var Match_pushed_at = result[x].pushed_at;        //assign the API value: "pushed_at" to "pushed_at" variable
+                        var dateMatched = Match_pushed_at.substr(0, 10);  //eleminate any text in the string that comes after the date and assign it to variable "datePushed"
+                        if(datePushed == dateMatched){                    //if the current value of loop 1 is equal to the value of loop 2,
+                                                                          // it means that the user again pushed code on this date. 
+                        count++;                                          //Add 1 to the "count" variable to document this.
+                        }        
+                    }
+                if(arr1v4.indexOf(datePushed) < 1){                       //if the date doesn't already exist in "arr1" array, it isn't a duplicate.
+                    matchv4.push(datePushed);                             //since it isn't a duplicate, push it to "Match" array, and
+                    arr2v4.push(count.toString())                         //push the number stored in the "count" variable to the "arr2" array
+                }
+                arr1v2.push(datePushed);                                  //no matter what, add datePushed to array 1. 
+                                                                          //All dates must be stored in this array so that the above IF statement can 
+                                                                          //cross reference datePushed with previous dates to determine whether it's a duplicate.
+            }
+                console.log(arr1v4);                                      //print result in log
+                console.log(matchv4);                                     //print result in log
+                console.log(arr2v4);                                      //print result in log
+            for (var z = 0; z < matchv4.length; z++) {                    // loop through the Match array
+                if(arr2v4[z] == "1"){
+                    $("#pushList4").append(                               //if arr2[z] is equal to 1,
+                     "<li>" + arr2v4[z] + " push on " + matchv4[z] + "</li>"                 //write "push" instead of "pushes" (just for proper english)
+                 );                                                       //also here, the main purpose is to print the number stored in arr2[z] in the view
+                }else{
+                    $("#pushList4").append(
+                     "<li>" + arr2v4[z] + " pushes on " + matchv4[z] + "</li>"
+                 );
+              }
+          }
         }
     });
        
@@ -119,5 +201,5 @@ $("#btn_get_repos").click(function() {
     new Chart(pushes).Line(pushData);
 
             // $("#repo_count").append("Total Repos: " + result.length);
-});
+}
 
